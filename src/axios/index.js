c84229527e647e2de46210c51d8434453ba31b21
Fedor Iskandarov <f.iskandarov@3commas.io>
2021-06-13 23:28:19 +0300

    upd

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const FIREBASE_UR = process.env.REACT_APP_FIREBASE_URL;
+const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
 
-export default axios.create({ baseURL: FIREBASE_UR });
+export default axios.create({ baseURL: FIREBASE_URL });
